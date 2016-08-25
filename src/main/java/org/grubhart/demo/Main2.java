76c60104bb273a70126523b20219d3ca433337c1
grubhart <grubhart@gmail.com>
2016-08-25 12:26:51 -0500

    agregados 2 archivos mas

--- src/main/java/org/grubhart/demo/Main2.java
+++ src/main/java/org/grubhart/demo/Main2.java
@@ -3,13 +3,13 @@ package org.grubhart.demo;
 /**
  * Created by Edson on 25/08/2016.
  */
-public class Main {
+public class Main2 {
 
     // test comentario de prueba
 
     public static void main(){
 
-        System.out.println("hola mundo1");
+        System.out.println("hola mundo");
         System.out.println("hola mundo2");
         System.out.println("hola mundo3");
 
---
+++ src/main/java/org/grubhart/demo/Main3.java
@@ -0,0 +1,20 @@
+package org.grubhart.demo;
+
+/**
+ * Created by Edson on 25/08/2016.
+ */
+public class Main3 {
+
+    // test comentario de prueba
+
+    public static void main(){
+
+        System.out.println("hola mundogh");
+        System.out.println("hola mundo4");
+        System.out.println("hola mundortg");
+
+
+
+    }
+
+}
