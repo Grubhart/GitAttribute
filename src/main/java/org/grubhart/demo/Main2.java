97fd81810314a403f778cb1baaff0a05a12e9eac
grubhart <grubhart@gmail.com>
2016-08-25 14:15:57 -0500

    añadido directorio gradle al repositorio

--- src/main/java/org/grubhart/demo/Main2.java
+++ src/main/java/org/grubhart/demo/Main2.java
@@ -12,8 +12,8 @@ public class Main2 {
         System.out.println("hola mundo");
         System.out.println("hola mundo2");
         System.out.println("hola mundo3");
-
-
+        System.out.println("hola mundo3");
+        System.out.println("hola mundo3");
 
     }
 
